1a6c2528a42c9a3e70bc770d246a14f79785baf8
wedamame <103207733+wedamame@users.noreply.github.com>
2022-04-08 02:05:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,23 @@
-from discord.ext import commands
-from os import getenv
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
+TOKEN = 'OTYwNTE4MzkwMTk4NDUyMjY0.YkrmdQ.a00OWTuo87KN2c1Tye0_s8WGoo0'
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    print('kp')
 
+@client.event
+async def on_message(message):
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    if message.author.bot:
+        return
+    if message.content == '!nae':
+        await message.channel.send("https://cdn.discordapp.com/attachments/603123201928855552/959143328258080828/407BD401-0C12-4642-B23A-9DF548B6F220.jpg")
+    if message.content == '!hk':
+        await message.channel.send("https://cdn.discordapp.com/attachments/603123201928855552/959152855548297287/DAE403F5-8773-4DCE-B9A6-C7318368CCC5.jpg")
+    if message.content == '!ikiri':
+        await message.channel.send("https://cdn.discordapp.com/attachments/603123201928855552/959144455368212570/5BA63EF3-96B7-4268-86B6-336158B99219.jpg")
 
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+client.run(TOKEN)
